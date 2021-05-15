edc2b7e0f0cc9ab9ac9e7a2d2b3c668432ea0d67
Gisele Paiva <giselepaiva.arq@gmail.com>
2021-05-15 18:58:34 -0300

    adição do script para salvar dados no local storage

---
+++ script.js
@@ -0,0 +1,63 @@
+let anuncio = document.getElementById("anuncio");
+let cliente = document.getElementById("cliente");
+let dataInicio = document.getElementById("dataInicio");
+let dataTermino = document.getElementById("dataTermino");
+let investimentoDia = document.getElementById("investimentoDia");
+
+function tempoTotal() {
+    let start = dataInicio.value;
+    let end = dataTermino.value;
+
+    start = new Date(start);
+    end = new Date(end); 
+
+    let qntTempo = Math.abs(end - start);
+    let tempoUmDia = 1000 * 60 * 60 * 24;
+    let qntDias = qntTempo / tempoUmDia;
+
+    return qntDias;
+}
+
+function cadastroAnuncio() {
+    if (anuncio.value == '' || cliente.value == '' || dataInicio.value == '' || dataTermino.value == '' || investimentoDia.value == '') {
+        alert('Por favor, complete o formulário!');
+    } else {
+        let dados = JSON.parse(localStorage.getItem("dadosAnuncio"));
+
+        if(dados == null) {
+            localStorage.setItem("dadosAnuncio", "[]");
+            dados = [];
+        }
+
+        let tempoAnuncio = tempoTotal();
+        let vTotalInvestido = investimentoDia.value * tempoAnuncio;
+        let maxVisual = investimentoDia.value * 86.41;
+        let maxClique = maxVisual * 0.12;
+        let maxComp = maxVisual * 0.018;
+
+
+        var salvarDados = {
+            nome: anuncio.value,
+            cliente: cliente.value,
+            investimentoDia: investimentoDia.value,
+            valorTotal: vTotalInvestido,
+            VisuMax: maxVisual,
+            cliqueMax: maxClique,
+            compMax: maxComp
+        }
+
+        dados.push(salvarDados);
+        localStorage.setItem("dadosAnuncio", JSON.stringify(dados));
+        alert("Registo incluído com sucesso!");
+
+        anuncio.value = "";
+        cliente.value = "";
+        dataInicio = "";
+        dataTermino = "";
+        investimentoDia = "";
+        vTotalInvestido = "";
+        maxVisual = "";
+        maxClique = "";
+        maxComp = "";
+    }
+}
